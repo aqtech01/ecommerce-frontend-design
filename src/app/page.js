9a9e9a42808f794c609d98326679c25096a929d3
aqtech <abdulqadeer0120@gmail.com>
2025-03-07 11:57:47 +0000

    Week one Task Complete

---
+++ src/app/page.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Main from "./components/Main"
+
+
+export default function Home() {
+  return (
+ <>
+ <Header/>
+ <Main></Main>
+ <Footer/>
+ 
+ </>
+  );
+}
